68620db5d73c8679d5a86ff287c888f79291ccc7
John Patrick Onyango <john.onyango.@scandiweb.com>
2024-07-12 19:07:16 +0300

    added line the end of files

--- app/code/Scandiweb/Test/registration.php
+++ app/code/Scandiweb/Test/registration.php
@@ -1,8 +1,10 @@
-<?php 
+<?php
 declare(strict_types=1);
 
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
     'Scandiweb_Test',
     __DIR__
 );
